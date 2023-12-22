115bfa910cfb7a4681b83cd88e0d4da49c74bbb8
Sunyvrm1 <115399549+Sunyvrm1@users.noreply.github.com>
2023-12-22 10:00:29 +0000

    22-12

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ function metretokilometer(meter) {
   return (meter * 3.6).toFixed(1);
 }
 
+//Vairable
 const city_name = document.getElementById("city_name");
 const temp = document.getElementById("temp");
 const temp_max = document.getElementById("temp_max");
@@ -33,6 +34,62 @@ const Cloudiness = document.getElementById("Cloudiness");
 const Visiblity = document.getElementById("Visiblity");
 const weather_des = document.getElementById("weather_des");
 const weather_img = document.querySelector(".weather_image img");
+
+//By default
+
+fetch(
+  `https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=beea3435003dac5511b106b3cd2c6350`
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    city_name.innerHTML = data.name;
+    temp.innerHTML = kelvinToCelsius(data.main.temp);
+    temp_max.innerHTML = kelvinToCelsius(data.main.temp_max);
+    temp_min.innerHTML = kelvinToCelsius(data.main.temp_min);
+    temp_feel.innerHTML = kelvinToCelsius(data.main.feels_like);
+    humdity.innerHTML = data.main.humidity;
+    wind.innerHTML = metretokilometer(data.wind.speed);
+    pressure.innerHTML = data.main.pressure;
+    Cloudiness.innerHTML = data.clouds.all;
+    Visiblity.innerHTML = data.visibility / 1000;
+    sunrise.innerHTML = unixToTime(data.sys.sunrise);
+    sunset.innerHTML = unixToTime(data.sys.sunset);
+    weather_des.innerHTML = data.weather[0].main;
+    weather_img.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+
+    const lat = data.coord.lat;
+    const lon = data.coord.lon;
+    const aqi_Condition = document.getElementById("aqi_Condition");
+    const aq_level = document.getElementById("aq_level");
+    fetch(
+      `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=beea3435003dac5511b106b3cd2c6350`
+    )
+      .then((res) => res.json())
+      .then((data1) => {
+        console.log(data1);
+        console.log(data1.list[0].main.aqi);
+        aqi_Index.innerHTML = data1.list[0].main.aqi;
+        if (aqi_Index.innerHTML == 1) {
+          aqi_Condition.innerHTML = "Good";
+          aq_level.style.left = "5%";
+        } else if (aqi_Index.innerHTML == 2) {
+          aqi_Condition.innerHTML = "Fair";
+          aq_level.style.left = "30%";
+        } else if (aqi_Index.innerHTML == 3) {
+          aqi_Condition.innerHTML = "Moderate";
+          aq_level.style.left = "50%";
+        } else if (aqi_Index.innerHTML == 4) {
+          aqi_Condition.innerHTML = "Poor";
+          aq_level.style.left = "70%";
+        } else {
+          aqi_Condition.innerHTML = "Very Poor";
+          aq_level.style.left = "90%";
+        }
+      });
+  });
+
+//click event
 
 const weatherBtn = document.getElementById("weatherBtn");
 weatherBtn.addEventListener("click", () => {
@@ -61,6 +118,22 @@ weatherBtn.addEventListener("click", () => {
         sunset.innerHTML = unixToTime(data.sys.sunset);
         weather_des.innerHTML = data.weather[0].main;
         weather_img.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+
+        //bg change
+        const body = document.querySelector("body");
+        if (data.weather[0].main === "Clouds") {
+          body.style.backgroundImage = 'url("/cloud.jpg")';
+        } else if (data.weather[0].main === "Clear") {
+          body.style.backgroundImage = 'url("/sunny.jpg")';
+        } else if (data.weather[0].main === "Snow") {
+          body.style.backgroundImage = 'url("/snow.jpg")';
+          document.querySelector("h1").style.color = "white";
+        } else if (data.weather[0].main === "Rain") {
+          body.style.backgroundImage = 'url("/rain.jpg")';
+        } else {
+          body.style.backgroundImage = 'url("/smoke.jpg")';
+        }
+        body.style.backgroundSize = "cover";
 
         const lat = data.coord.lat;
         const lon = data.coord.lon;
@@ -95,3 +168,5 @@ weatherBtn.addEventListener("click", () => {
       });
   }
 });
+
+// fgfgjfgjghdskfskdjf
